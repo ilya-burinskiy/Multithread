c9bc14d0aea520fcabf0baa6325716f70303b788
Ilya <burinskiy992@gmail.com>
2020-08-02 03:37:47 +0300

    added a limit on the number of people in the bathroom

--- shared_shower.py
+++ shared_shower.py
@@ -10,6 +10,8 @@ from random import randint
 #       (nm == 0 && nw == 0) || (nm == 0 && turn == 'W') ||
 #       (nw == 0 && turn == 'M')
 
+MAX_COUNT = 2       # maximum number of people in the shower
+
 N = 6               # threads num
 M = N // 2 + N % 2  # number of men
 W = N // 2          # number of women
@@ -17,96 +19,141 @@ W = N // 2          # number of women
 nm = 0              # number of men in shower
 nw = 0              # number of women in shower
 turn = None         # whose turn to use the shower
-is_set = False      # is turn is set
 
 e = Semaphore(1)    # for entring in critical area
 m = Semaphore(0)    # for pause man
 w = Semaphore(0)    # for pause woman
+sm = Semaphore(0)   # for pause man who took a shower
+sw = Semaphore(0)   # for woman who took a shower
 
 dm = 0              # number of suspended men
 dw = 0              # number of suspended women
+dsm = 0             # number of man who took a shower
+dsw = 0             # nubmer of woman who took a shower
 
-def set_turn(s):
-    global e, is_set, turn
-    e.acquire()
-    if not is_set:
-        turn = s
-    e.release()
+def man(id_):
+    global nm, nw, turn, e, m, w, sm, sw, dm, dw, dsm, dsw
 
-def man():
-    global nm, nw, turn, e, m, w, dm, dw
-    set_turn('M')
     while True:
+
+        # Enter protocol
         e.acquire()
-        if nw > 0 or turn == 'W':
+        if nw > 0 or turn == 'W' or nm == MAX_COUNT:
             dm += 1
             e.release()
             m.acquire()
         nm += 1
-        if nm == M:
+        if nm == min(MAX_COUNT, M):
             turn = 'W'
-        print('nw = %d, nm = %d' % (nw, nm))
-        if dm > 0:
+        print('nw: %d nm: %d id: %d' % (nw, nm, id_))
+
+        # SIGNAL
+        if dm > 0 and nm < MAX_COUNT:
             dm -= 1
             m.release()
+        elif dsm > 0 and nm < MAX_COUNT:
+            dsm -= 1
+            sm.release()
+            e.release()
         else:
             e.release()
+
+        # Showering
         sleep(1)
 
+        # Exit protocol
         e.acquire()
         nm -= 1
-        print('nw = %d, nm = %d' % (nw, nm))
-
-        if nm == 0 and dw > 0:
-            dw -= 1
-            w.release()
-        elif dm > 0 and turn == 'M':
-            dm -= 1
-            m.release()
+        dsm += 1
+        # print('nw: %d nm: %d id: %d' % (nw, nm, id_))
+        if nm == 0:
+            if dw > 0:
+                dw -= 1
+                w.release()
+            elif dsw > 0:
+                dsw -= 1
+                sw.release()
+                e.release()
+        elif turn == 'M':
+            if dm > 0 and nm < MAX_COUNT:
+                dm -= 1
+                m.release()
+            elif dsm > 0 and nm < MAX_COUNT:
+                dsm -= 1
+                sm.release()
+                e.release()
         else:
             e.release()
+        
+        sm.acquire()
 
-def woman():
-    global nm, nw, turn, e, m, w, dm, dw
-    set_turn('W')
+def woman(id_):
+    global nm, nw, turn, e, m, w, sm, sw, dm, dw, dsm, dsw
+
     while True:
+
+        # Enter protocol
         e.acquire()
-        if nm > 0 or turn == 'M':
+        if nm > 0 or turn == 'M' or nw == MAX_COUNT:
             dw += 1
             e.release()
             w.acquire()
         nw += 1
-        if nw == W:
+        if nw == min(MAX_COUNT, W):
             turn = 'M'
-        print('nw = %d, nm = %d' % (nw, nm))
-        if dw > 0:
+        print('nw: %d nm: %d id: %d' % (nw, nm, id_))
+
+        # SIGNAL
+        if dw > 0 and nw < MAX_COUNT:
             dw -= 1
             w.release()
+        elif dsw > 0 and nw < MAX_COUNT:
+            dsw -= 1
+            sw.release()
+            e.release()
         else:
             e.release()
 
+        # Showering
         sleep(1)
 
+        # Exit protocol
         e.acquire()
         nw -= 1
-        print('nw = %d, nm = %d' % (nw, nm))
+        dsw += 1
+        # print('nw: %d nm: %d id: %d' % (nw, nm, id_))
 
-        if nw == 0 and dm > 0:
-            dm -= 1
-            m.release()
-        elif dw > 0 and turn == 'W':
-            dw -= 1
-            w.release()
+        if nw == 0:
+            if dm > 0:
+                dm -= 1
+                m.release()
+            elif dsm > 0:
+                dsm -= 1
+                sm.release()
+                e.release()
+        elif turn == 'W':
+            if dw > 0 and nw < MAX_COUNT:
+                dw -= 1
+                w.release()
+            elif dsw > 0 and nw < MAX_COUNT:
+                dsw -= 1
+                sw.release()
+                e.release()
         else:
             e.release()
+        
+        sw.acquire()
 
 def main():
+    global turn
+    turn = ['M', 'W'][randint(0, 1)]
+
     threads = [None for _ in range(N)]
     for i in range(N):
         if i % 2 == 0:
-            threads[i] = Thread(target=man)
+            threads[i] = Thread(target=man, args=(i, ))
         else:
-            threads[i] = Thread(target=woman)
+            threads[i] = Thread(target=woman, args=(i, ))
         threads[i].start()
 
     for i in range(N):
